89932e68b1531c69ff61f3f7626b3988ddc48147
The-SOU <kooshal@naver.com>
2021-04-07 00:35:53 +0900

    first commit

--- app/src/main/java/com/example/modaktestone/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/modaktestone/navigation/DetailViewFragment.kt
@@ -64,13 +64,29 @@ class DetailViewFragment : Fragment() {
                 ?.replace(R.id.main_content, fragment)?.commit()
         }
 
+        //추천 정보 관련 내용들
+        var requestItem: ArrayList<Int> = arrayListOf(
+            R.drawable.information_first,
+            R.drawable.information_second,
+            R.drawable.information_third
+        )
+        var titleItem: ArrayList<String> = arrayListOf(
+            "네이버 인턴의 첫 장보기 스토리",
+            "미래에셋 증권 이벤트",
+            "영화 <레 미제라블> 시사회 이벤트"
+        )
+        var contentItem: ArrayList<String> = arrayListOf(
+            "네이버 장보기로 최대 10% 적립 받고,\n멤버십 1개월 무료 이용하세요!",
+            "주식의 시작은 미래에셋에서\n케잌과 커피 한 잔 받아가세요!",
+            "<레 미제라블> 예고편 감상 후 기대평을 써주세요!\n추첨을 통해 시사회에 초대합니다!"
+        )
 
-
-        var requestItem : ArrayList<Int> = arrayListOf(R.drawable.ic_calendar, R.drawable.ic_house, R.drawable.ic_speaker)
-
-        binding.viewPager.adapter = ViewPagerAdapter(requestItem)
+        //추천정보 뷰페이저 어댑터
+        binding.viewPager.adapter = ViewPagerAdapter(requestItem, titleItem, contentItem)
         binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
+        //
+        binding.dotsIndicator.setViewPager2(binding.viewPager)
 
 
 
@@ -83,9 +99,6 @@ class DetailViewFragment : Fragment() {
 //        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
-    private fun getItemList(): ArrayList<Int> {
-        return arrayListOf<Int>(R.drawable.ic_calendar, R.drawable.ic_house, R.drawable.ic_speaker)
-    }
 
     inner class RepeatboardRecyclerViewAdapter :
         RecyclerView.Adapter<RepeatboardRecyclerViewAdapter.CustomViewHolder>() {
@@ -415,25 +428,44 @@ class DetailViewFragment : Fragment() {
 
     }
 
-    inner class ViewPagerAdapter(itemList: ArrayList<Int>): RecyclerView.Adapter<ViewPagerAdapter.CustomViewHolder>() {
+    inner class ViewPagerAdapter(
+        itemList: ArrayList<Int>,
+        titleList: ArrayList<String>,
+        contentList: ArrayList<String>
+    ) :
+        RecyclerView.Adapter<ViewPagerAdapter.CustomViewHolder>() {
         var itemDTO = itemList
 
-        inner class CustomViewHolder(val binding: ItemPagerBinding) : RecyclerView.ViewHolder(binding.root)
+        var titleDTO = titleList
+
+        var contentDTO = contentList
+
+        inner class CustomViewHolder(val binding: ItemPagerBinding) :
+            RecyclerView.ViewHolder(binding.root)
+
         override fun onCreateViewHolder(
             parent: ViewGroup,
             viewType: Int
         ): ViewPagerAdapter.CustomViewHolder {
-            val binding = ItemPagerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            val binding =
+                ItemPagerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
             return CustomViewHolder(binding)
         }
 
         override fun onBindViewHolder(holder: ViewPagerAdapter.CustomViewHolder, position: Int) {
             holder.binding.imgPager.setImageResource(itemDTO[position])
+
+            holder.binding.pagerTvTitle.text = titleDTO[position]
+
+            holder.binding.pagerTvContent.text = contentDTO[position]
+
+
         }
 
         override fun getItemCount(): Int {
             return itemDTO.size
         }
+
 
     }
 }
